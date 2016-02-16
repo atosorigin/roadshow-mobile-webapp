9b409414fcc2834ca33ad57f2918e24022e04734
Mike Williams <mike.williams@atos.net>
2016-02-16 17:27:08 +0000

    fix webservice

--- src/main/java/net/atos/mobile/roadshow/ws/SubmitService.java
+++ src/main/java/net/atos/mobile/roadshow/ws/SubmitService.java
@@ -1,13 +1,17 @@
 package net.atos.mobile.roadshow.ws;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 @Path("/upload")
 public class SubmitService {
 
+	@GET
+	@Produces("text/html")
 	@Path("/test")
 	public Response test() {
-		return Response.ok("Test worked!").build();
+		return Response.ok("<h1>Test worked!</h1>").build();
 	}
 }
